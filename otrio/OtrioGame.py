a2ba01a9750621a4af25d4e42e164dc6021ed9cd
v! <149035689+s27x4@users.noreply.github.com>
2025-07-28 23:49:13 +0900

    fix player index

--- otrio/OtrioGame.py
+++ otrio/OtrioGame.py
@@ -73,7 +73,7 @@ class OtrioGame(Game):
         size, rem = divmod(action, 9)
         row, col = divmod(rem, 3)
 
-        idx = player - 1
+        idx = 0 if player == 1 else 1
         colors = self.player_colors[idx]
         c_idx = self.next_color[idx] if self.n_players == 2 else 0
         color = colors[c_idx]
@@ -98,7 +98,7 @@ class OtrioGame(Game):
         Returns:
             mask (np.ndarray[int8]): shape (27,), 1 = legal
         """
-        idx = player - 1
+        idx = 0 if player == 1 else 1
         colors = self.player_colors[idx]
         c_idx = self.next_color[idx] if self.n_players == 2 else 0
         color = colors[c_idx]
